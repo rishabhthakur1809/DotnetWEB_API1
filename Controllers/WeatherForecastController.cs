e5fe4fdef156be258dbfda7531b22edadc8cc993
rishabhthakur1809 <40190979+rishabhthakur1809@users.noreply.github.com>
2022-04-12 02:42:07 -0700

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DotnetWEB_API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "FreezingPart-1", "Bracing", "Chilly", "Cool", "MildPart-2", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
